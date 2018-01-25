87e6caf4fdc69d5fdba316edd12d31e9cb6eae81
knazark <knazark@tenantcloud.com>
2018-01-25 21:28:12 +0200

    add: New project

---
+++ app/app.js
@@ -0,0 +1,20 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('app', [])
+		.config(configure);
+
+
+	function configure() {
+		// $stateProvider
+		// 	.state('bookmark', {
+		// 		url: '',
+		// 		abstract: true
+		// 	})
+		// ;
+
+		// $urlRouterProvider.otherwise('/');
+	}
+
+})();
